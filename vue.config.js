79913d8db3c234dee105bc5eab41f87b5b096d18
hazuzuki <h2.onthestomach22@gmail.com>
2021-11-05 17:48:33 +0900

    change vue config path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    publicPath: '/animal_typing', //リポジトリ名を指定
+    publicPath: './', //リポジトリ名を指定
     outputDir: 'docs', //GitHubPagesで公開するにはdistではなくdocs配下にコンパイルする
+    assetsDir: './',
 }
