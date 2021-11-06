ab668b841fcc284cd1633fec3c2d47206e86633a
hazuzuki <h2.onthestomach22@gmail.com>
2021-11-06 20:19:32 +0900

    add ad

--- src/components/TypingTop.vue
+++ src/components/TypingTop.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-        <div><img src="@/assets/tree_animals_group (1).png" alt=""></div>
+        <div><img src="@/assets/tree_animals_group (1).png" alt="動物の画像" width="35%" height="35%"></div>
         <div class="start">Enterを押して開始</div>
     </div>
 </template>
